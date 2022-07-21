ae5ca97a508f81e96f9aa8db3abb665ba666a835
ShaimaaHossam <40139825+ShaimaaHossam@users.noreply.github.com>
2022-07-21 11:35:32 +0200

    modified places task

--- Session 8/places_task/js/script.js
+++ Session 8/places_task/js/script.js
@@ -9,50 +9,25 @@ $(document).ready(function () {
     })
 
     function createHTML(place) {
-        var place_container = document.createElement("div")
-        place_container.classList.add("col-12", "col-lg-3", "col-md-6")
-        var link = document.createElement("a")
-        link.style.textDecoration = "none"
-        link.rel = "modal:open"
-        link.id = place.id
-        link.href="#modal"
-        place_container.append(link)
-
-        var img = document.createElement("img")
-        img.classList.add("w-100", "h-50")
-        img.src = place.image
-        link.append(img)
-
-        var text_container = document.createElement("div")
-        text_container.classList.add("text-center", "back-light", "w-100", "px-3", "pt-1", "h-50", "d-flex", "flex-column", "justify-content-between")
-
-        var name = document.createElement("p")
-        name.classList.add("text-uppercase", "header-color", "fw-bold", "m-0", "mt-1")
-        name.innerHTML = place.name
-        text_container.append(name)
-
-        var brief = document.createElement("p")
-        brief.classList.add("fw-bold", "text-color", "subtext", "mt-1")
-        brief.innerHTML = place.brief
-        text_container.append(brief)
-
-        var details_container = document.createElement("div")
-        details_container.classList.add("d-flex", "date-text", "text-color", "justify-content-between", "mt-2")
-        
-        var date_container = document.createElement("div")
-        date_container.classList.add("d-flex", "align-items-baseline")
-        date_container.innerHTML ="<i class='fa-regular fa-calendar me-1'></i><p>"+place.date+"</p>"
-
-        var comments_container = document.createElement("div")
-        comments_container.classList.add("d-flex", "align-items-baseline")
-        comments_container.innerHTML ="<i class='fa-regular fa-calendar me-1'></i><p>"+place.comments+" comments</p>"
-        details_container.append(date_container)
-        details_container.append(comments_container)
-        text_container.append(details_container)
-        link.append(text_container)
-
-                
-        $("#places").append(place_container)
+        $("#places").append(`
+            <div class="col-md-6 col-lg-3 col-12">
+                <a rel="modal:open" id="${place.id}" href="#modal">
+                    <img class="w-100 h-50" src="${place.image}" />
+                    <div class="text-center back-light w-100 px-3 pt-1 h-50 d-flex flex-column justify-content-between">
+                        <p class="text-uppercase header-color fw-bold m-0">${place.name}</p>
+                        <p class="subtext fw-bold text-color">${place.brief}</p>
+                        <div class="d-flex date-text text-color justify-content-between">
+                            <div class="d-flex align-items-baseline">
+                            <i class='fa-regular fa-calendar me-1'></i><p>${place.date}</p>
+                            </div>
+                            <div class="d-flex align-items-baseline">
+                            <i class='fa-regular fa-calendar me-1'></i><p>${place.comments} comments</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        `)
     }
 
     function createModal(place){
